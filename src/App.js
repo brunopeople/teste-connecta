68a5944aaa303c129a9d864759f8bbb61859c0b7
bruno pessoa nunes de melo <brunopessoa.melo@gmail.com>
2021-09-27 19:49:53 -0300

    aplicando a folha de estilo e criando o layout para a tela de login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './App.css';
 import Dashboard from '../src/components/Dashboard/Dashboard';
+import Login from '../src/components/Login/Login';
 import Preferences from '../src/components/Preferences/Preferences';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
